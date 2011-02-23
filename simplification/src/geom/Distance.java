39f830a074b298e1befb22f498ef810cb7ba60a2
ceikute <ceikute@beab7c72-00ed-11df-998c-3ffb47665784>
2011-02-23 13:18:45 +0000

    imaginary point and count of unique timepoints

---
+++ simplification/src/geom/Distance.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package geom;
+
+/**
+ *
+ * @author ceikute
+ */
+public class Distance {
+    int trajId_1;
+    int trajId_2;
+    double distance;
+    String timeFromStr;
+    String timeToStr;
+    double timeFrom;
+    double timeTo;
+
+    
+}
